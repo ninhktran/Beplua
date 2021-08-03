f0f28e7527ddc9f949ddd07702a71302daad2226
Ninh Tran <tran1108@umn.edu>
2021-08-03 16:06:26 -0500

    Initial Commit

---
+++ Beplua/BepluaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BepluaApp.swift
+//  Beplua
+//
+//  Created by Ninh Tran on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BepluaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
